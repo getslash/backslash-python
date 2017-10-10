392c339a3f8f96d32c038db18aad86fc575b7efb
Rotem Yaari <vmalloc@gmail.com>
2017-10-10 17:17:10 +0300

    Fix pylint errors

--- backslash/timing_container.py
+++ backslash/timing_container.py
@@ -12,10 +12,10 @@ class TimingContainer(object):
         self.client.api.call_function('report_timing_{}'.format(start_stop), kwargs) # pylint: disable=no-member
 
     def _get_identity_kwargs(self):
-        if self.type.lower() == 'session':
-            return {'session_id': self.id}
+        if self.type.lower() == 'session': # pylint: disable=no-member
+            return {'session_id': self.id} # pylint: disable=no-member
 
-        return {'test_id': self.id, 'session_id': self.session_id}
+        return {'test_id': self.id, 'session_id': self.session_id} # pylint: disable=no-member
 
     def get_timings(self):
-        return self.client.api.call.get_timings(**self._get_identity_kwargs())
+        return self.client.api.call.get_timings(**self._get_identity_kwargs()) # pylint: disable=no-member
